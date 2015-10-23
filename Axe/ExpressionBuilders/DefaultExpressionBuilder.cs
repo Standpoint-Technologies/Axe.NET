04e7ead6c1d54406981a14c6cad40bc2eeb268a1
Tyler Benfield <rtbenfield@gmail.com>
2015-10-23 16:08:37 -0400

    Adding support for collections :boom:

--- Axe/ExpressionBuilders/DefaultExpressionBuilder.cs
+++ Axe/ExpressionBuilders/DefaultExpressionBuilder.cs
@@ -65,38 +65,112 @@ namespace Axe.ExpressionBuilders
         /// <param name="profile"></param>
         /// <param name="parentParameter"></param>
         /// <returns></returns>
-        protected MemberInitExpression RecursiveExpressionBuilder<TEntity>(FieldRing fieldRing, AxeProfile profile, Expression parentParameter)
+        protected Expression RecursiveExpressionBuilder<TEntity>(FieldRing fieldRing, AxeProfile profile, Expression parentParameter, int counter = 1)
         {
-            var newType = profile.ExtendTypesDynamically ? GetOrCreateExtendingType(typeof(TEntity)) : typeof(TEntity);
+            Type oldType = typeof(TEntity);
+            Type collectionType = null;
+
+            // If type is a collection, we need the inner type
+            var isCollection = false;
+            var ienumerable = oldType.IsGenericType && oldType.GetGenericTypeDefinition() == typeof(IEnumerable<>) ? oldType : oldType.GetInterfaces()
+                .Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                .FirstOrDefault();
+            var inputParameter = parentParameter;
+            if (ienumerable != null)
+            {
+                isCollection = true;
+                collectionType = oldType;
+                oldType = ienumerable.GetGenericArguments().First();
+                inputParameter = Expression.Parameter(oldType, $"input{counter}");
+            }
+
+            var newType = profile.ExtendTypesDynamically ? GetOrCreateExtendingType(oldType) : oldType;
             var propertyBindingFlags = profile.IgnoreCase ? BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance : BindingFlags.Public | BindingFlags.Instance;
 
             var newExpression = Expression.New(newType);
 
             var bindings = fieldRing.Fields.Select(field =>
             {
-                var propertyInfo = typeof(TEntity).GetProperty(field, propertyBindingFlags);
+                var propertyInfo = oldType.GetProperty(field, propertyBindingFlags);
                 if (propertyInfo != null)
                 {
-                    var propertyExpression = Expression.Property(parentParameter, propertyInfo);
+                    var propertyExpression = Expression.Property(inputParameter, propertyInfo);
                     return Expression.Bind(propertyInfo, propertyExpression);
                 }
                 return null;
             }).Union(fieldRing.NestedRings.Select(field =>
             {
-                var propertyInfo = typeof(TEntity).GetProperty(field.Key, propertyBindingFlags);
+                var propertyInfo = oldType.GetProperty(field.Key, propertyBindingFlags);
                 if (propertyInfo != null)
                 {
-                    var nestedParameter = Expression.Property(parentParameter, field.Key);
-                    var propertyExpression = (MemberInitExpression)typeof(DefaultExpressionBuilder)
+                    var nestedParameter = Expression.Property(inputParameter, field.Key);
+                    var propertyExpression = (Expression)typeof(DefaultExpressionBuilder)
                         .GetMethod("RecursiveExpressionBuilder", BindingFlags.NonPublic | BindingFlags.Instance)
                         .MakeGenericMethod(propertyInfo.PropertyType)
-                        .Invoke(this, new object[] { field.Value, profile, nestedParameter });
+                        .Invoke(this, new object[] { field.Value, profile, nestedParameter, counter + 1 });
                     return Expression.Bind(propertyInfo, propertyExpression);
                 }
                 return null;
             })).Where(x => x != null);
 
-            return Expression.MemberInit(newExpression, bindings);
+            if (isCollection)
+            {
+                var initExpression = Expression.MemberInit(newExpression, bindings);
+
+                var selectExpression = getSelectExpression(oldType, newType, parentParameter, initExpression, (ParameterExpression)inputParameter, $"input{counter}");
+
+                if (isList(collectionType, oldType))
+                {
+                    return getToListCallExpression(parentParameter.Type, oldType, selectExpression);
+                }
+                else if (collectionType.IsArray)
+                {
+                    return getToArrayCallExpression(oldType, selectExpression);
+                }
+
+                return selectExpression;
+            }
+            else
+            {
+                return Expression.MemberInit(newExpression, bindings);
+            }
+        }
+
+        private static Expression getSelectExpression(Type sourceType, Type destinationType, Expression source, MemberInitExpression initExpression, ParameterExpression instanceParameter, string parameterName)
+        {
+            var delegateType = typeof(Func<,>).MakeGenericType(sourceType, destinationType);
+            var newLambdaExpression = Expression.Lambda(delegateType, initExpression, instanceParameter);
+
+            return Expression.Call(
+                typeof(Enumerable),
+                "Select",
+                new Type[] { sourceType, destinationType },
+                source,
+                newLambdaExpression);
+        }
+
+        private static MethodCallExpression getToArrayCallExpression(Type destinationType, Expression selectExpression)
+        {
+            return Expression.Call(
+                typeof(Enumerable),
+                "ToArray",
+                new[] { destinationType },
+                selectExpression);
+        }
+
+        private static MethodCallExpression getToListCallExpression(Type collectionType, Type destinationType, Expression selectExpression)
+        {
+            return Expression.Call(
+                typeof(Enumerable),
+                collectionType.IsArray ? "ToArray" : "ToList",
+                new[] { destinationType },
+                selectExpression);
+        }
+
+        private static bool isList(Type collectionType, Type sourceType)
+        {
+            return typeof(IList<>).MakeGenericType(sourceType).IsAssignableFrom(collectionType)
+                || typeof(ICollection<>).MakeGenericType(sourceType).IsAssignableFrom(collectionType);
         }
     }
 }
---
+++ AxeTests/ExpressionBuilderTests/CollectionTests.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Axe.ExpressionBuilders;
+using Axe.FieldParsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AxeTests.ExpressionBuilderTests
+{
+    [TestClass]
+    public class CollectionTests
+    {
+        private DefaultExpressionBuilder _builder;
+        private Axe.FieldRing _fieldRing;
+        private Axe.AxeProfile _profile;
+
+
+        [TestInitialize]
+        public void InitializeExpressionBuilder()
+        {
+            _builder = new DefaultExpressionBuilder();
+            _fieldRing = new Axe.FieldRing()
+            {
+                Fields = new List<string>() { "id" },
+                NestedRings = new Dictionary<string, Axe.FieldRing>()
+                {
+                    {"items", new Axe.FieldRing()
+                        {
+                            Fields = new List<string>() { "id" }
+                        }
+                    }
+                }
+            };
+            _profile = new Axe.AxeProfile()
+            {
+                ExpressionBuilder = _builder,
+                ExtendTypesDynamically = true,
+                FieldParser = new GoogleParser(),
+                IgnoreCase = true
+            };
+        }
+
+
+        [TestMethod]
+        public void TestEnumerable()
+        {
+            var expression = _builder.BuildExpression<TestEnumerableClass>(_fieldRing, _profile);
+
+            var testObject = new TestEnumerableClass
+            {
+                ID = 1,
+                Items = new List<TestChildObject>
+                {
+                    new TestChildObject
+                    {
+                        ID = 2,
+                        Exclude = "should not display"
+                    }
+                }
+            };
+
+            var result = expression.Compile()(testObject);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testObject.ID, result.ID);
+            Assert.IsNotNull(result.Items);
+            Assert.IsTrue(result.Items.Count() == 1);
+
+            var childResult = result.Items.First();
+            Assert.AreEqual(2, childResult.ID);
+            Assert.IsNull(childResult.Exclude);
+        }
+
+
+        [TestMethod]
+        public void TestList()
+        {
+            var expression = _builder.BuildExpression<TestListClass>(_fieldRing, _profile);
+
+            var testObject = new TestListClass
+            {
+                ID = 1,
+                Items = new List<TestChildObject>
+                {
+                    new TestChildObject
+                    {
+                        ID = 2,
+                        Exclude = "should not display"
+                    }
+            }
+            };
+
+            var result = expression.Compile()(testObject);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testObject.ID, result.ID);
+            Assert.IsNotNull(result.Items);
+            Assert.IsTrue(result.Items.Count() == 1);
+
+            var childResult = result.Items.First();
+            Assert.AreEqual(2, childResult.ID);
+            Assert.IsNull(childResult.Exclude);
+        }
+
+
+        [TestMethod]
+        public void TestArray()
+        {
+            var expression = _builder.BuildExpression<TestArrayClass>(_fieldRing, _profile);
+
+            var testObject = new TestArrayClass
+            {
+                ID = 1,
+                Items = new TestChildObject[]
+                {
+                    new TestChildObject
+                    {
+                        ID = 2,
+                        Exclude = "should not display"
+                    }
+                }
+            };
+
+            var result = expression.Compile()(testObject);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testObject.ID, result.ID);
+            Assert.IsNotNull(result.Items);
+            Assert.IsTrue(result.Items.Count() == 1);
+
+            var childResult = result.Items.First();
+            Assert.AreEqual(2, childResult.ID);
+            Assert.IsNull(childResult.Exclude);
+        }
+
+
+        [TestMethod]
+        public void TestCollection()
+        {
+            var expression = _builder.BuildExpression<TestCollectionClass>(_fieldRing, _profile);
+
+            var testObject = new TestCollectionClass
+            {
+                ID = 1,
+                Items = new List<TestChildObject>
+                {
+                    new TestChildObject
+                    {
+                        ID = 2,
+                        Exclude = "should not display"
+                    }
+                }
+            };
+
+            var result = expression.Compile()(testObject);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testObject.ID, result.ID);
+            Assert.IsNotNull(result.Items);
+            Assert.IsTrue(result.Items.Count() == 1);
+
+            var childResult = result.Items.First();
+            Assert.AreEqual(2, childResult.ID);
+            Assert.IsNull(childResult.Exclude);
+        }
+
+
+        public class TestChildObject
+        {
+            public int ID { get; set; }
+
+            public string Exclude { get; set; }
+        }
+
+        public class TestEnumerableClass
+        {
+            public int ID { get; set; }
+
+            public IEnumerable<TestChildObject> Items { get; set; }
+        }
+
+        public class TestListClass
+        {
+            public int ID { get; set; }
+
+            public List<TestChildObject> Items { get; set; }
+        }
+
+        public class TestArrayClass
+        {
+            public int ID { get; set; }
+
+            public TestChildObject[] Items { get; set; }
+        }
+
+        public class TestCollectionClass
+        {
+            public int ID { get; set; }
+
+            public ICollection<TestChildObject> Items { get; set; }
+        }
+    }
+}
